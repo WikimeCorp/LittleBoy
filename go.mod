0d2b60ff2cb8a1370e83d3dab34bfe6310827080
kik0908 <kik0908@yandex.ru>
2022-12-03 22:53:11 +0300

    Done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/WikimeCorp/LittleBoy
+
+go 1.18
+
+require github.com/JeremyLoy/config v1.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	conflib "github.com/JeremyLoy/config"
+)
+
+type configStruct struct {
+	VkClientID     string `config:"REACT_APP_CLIENT_ID"`
+	VkAuthURI      string `config:"REACT_APP_VKAUTH_URI"`
+	VkClientSecret string `config:"REACT_APP_CLIENT_SECRET"`
+	RedirectURI    string `config:"REACT_APP_REDIRECT_URI"`
+	Port           string `config:"LITTLE_BOY_PORT"`
+}
+
+var config = configStruct{}
+
+func getToken(code string) string {
+	url := fmt.Sprintf("%s/access_token?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s",
+		config.VkAuthURI, code, config.RedirectURI, config.VkClientID, config.VkClientSecret)
+	req, _ := http.NewRequest("POST", url, nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	token := struct {
+		AccessToken string `json:"access_token"`
+	}{}
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(bytes, &token)
+	return token.AccessToken
+}
+
+func main() {
+	configPath := flag.String("configPath", ".env", "Path to config file.")
+
+	err := conflib.From(*configPath).FromEnv().To(&config)
+	if err != nil {
+		log.Fatal("Config read error:", err)
+	}
+
+	http.HandleFunc("/access_token", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			http.Error(w, "code is empty", http.StatusBadRequest)
+			return
+		}
+
+		token := getToken(code)
+		if token == "" {
+			http.Error(w, "invalide code", http.StatusBadRequest)
+			return
+		}
+		ans := struct {
+			Token string `json:"token"`
+		}{token}
+		w.Header().Set("Content-Type", "application/json")
+		ansBytes, _ := json.Marshal(ans)
+		w.Write(ansBytes)
+
+	})
+	http.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
+		url := fmt.Sprintf("https://oauth.vk.com/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s", config.VkClientID, config.RedirectURI, "12345")
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(r.URL.Query().Get("code")))
+	})
+
+	fmt.Println(config)
+	fmt.Println("Server start")
+	http.ListenAndServe(":"+config.Port, nil)
+}
