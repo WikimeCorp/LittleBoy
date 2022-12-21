39a23c5c59ebea6b5360883d11337226a7138fd0
kik0908 <kik0908@yandex.ru>
2022-12-21 05:10:13 +0300

    Docker end

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/WikimeCorp/LittleBoy
 
 go 1.18
 
-require github.com/JeremyLoy/config v1.5.0 // indirect
+require (
+	github.com/JeremyLoy/config v1.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 
 	conflib "github.com/JeremyLoy/config"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 type configStruct struct {
@@ -16,7 +18,7 @@ type configStruct struct {
 	VkAuthURI      string `config:"REACT_APP_VKAUTH_URI"`
 	VkClientSecret string `config:"REACT_APP_CLIENT_SECRET"`
 	RedirectURI    string `config:"REACT_APP_REDIRECT_URI"`
-	Port           string `config:"LITTLE_BOY_PORT"`
+	Port           string `config:"REACT_APP_LITTLE_BOY_PORT"`
 }
 
 var config = configStruct{}
@@ -27,27 +29,37 @@ func getToken(code string) string {
 	req, _ := http.NewRequest("POST", url, nil)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Println(err)
+		log.Println("ERR: ", err)
 		return ""
 	}
 	defer resp.Body.Close()
 	token := struct {
 		AccessToken string `json:"access_token"`
 	}{}
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	json.Unmarshal(bytes, &token)
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("ERR BODY READING ", err)
+	}
+	log.Println("vk answer ", string(bytes))
+	err = json.Unmarshal(bytes, &token)
+	if err != nil {
+		log.Println("ERR UNMARSHAL ", err)
+	}
 	return token.AccessToken
 }
 
 func main() {
-	configPath := flag.String("configPath", ".env", "Path to config file.")
+	configPath := flag.String("configPath", "empty.env", "Path to config file.")
 
 	err := conflib.From(*configPath).FromEnv().To(&config)
 	if err != nil {
 		log.Fatal("Config read error:", err)
 	}
 
-	http.HandleFunc("/access_token", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/access_token", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("/access_token enter")
 		code := r.URL.Query().Get("code")
 		if code == "" {
 			http.Error(w, "code is empty", http.StatusBadRequest)
@@ -64,10 +76,29 @@ func main() {
 		}{token}
 		w.Header().Set("Content-Type", "application/json")
 		ansBytes, _ := json.Marshal(ans)
+
 		w.Write(ansBytes)
+		fmt.Println("/access_token exit")
 	})
 
+	// router.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
+	// 	url := fmt.Sprintf("https://oauth.vk.com/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s", config.VkClientID, config.RedirectURI, "12345")
+	// 	http.Redirect(w, r, url, http.StatusSeeOther)
+	// })
+
+	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte(r.URL.Query().Get("code")))
+	// })
+
 	fmt.Println(config)
-	fmt.Println("Server start")
-	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
+	fmt.Println("Server start on " + "0.0.0.0:" + config.Port)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+config.Port, middleware(handlers.CORS()(router))))
+}
+
+func middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("middleware enter")
+		h.ServeHTTP(w, r)
+		fmt.Println("middleware end")
+	})
 }
